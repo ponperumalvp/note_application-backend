d8287e050a84107dc19d000d59d4dcb53cd88cd4
ponperumalvp <ponperimal1202@gmail.com>
2024-01-31 15:22:38 +0530

    new change

--- src/routes/bussinessNoteRoute.js
+++ src/routes/bussinessNoteRoute.js
@@ -8,8 +8,8 @@ import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-router.post("/createBussinessNote", createBussinessNote);
+router.post("/createBussinessNote", verifyToken, createBussinessNote);
 router.get("/getBussinessNote/:userId", getBussinessNote);
-router.delete("/deleteBussinessNote/:id", deleteBussinessNote);
+router.delete("/deleteBussinessNote/:id", verifyToken, deleteBussinessNote);
 
 export { router as bussinessNoteRouter };
--- src/routes/noteRoute.js
+++ src/routes/noteRoute.js
@@ -5,8 +5,8 @@ import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-router.post("/createNote", createNote);
+router.post("/createNote", verifyToken, createNote);
 router.get("/getNote/:userId", getNote);
-router.delete("/deleteNote/:id", deleteNote);
+router.delete("/deleteNote/:id", verifyToken, deleteNote);
 
 export { router as noteRouter };
